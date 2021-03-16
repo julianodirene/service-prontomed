306959c3cc89562af4e5039e33cd96f766066903
Juliano N. Direne <julianodirene@msn.com>
2021-03-16 18:53:24 -0300

    feature/heroku Changing env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,16 @@ import { PatientsModule } from './patients/patients.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['dist/**/*.entity.js'],
+      synchronize: false,
+    }),
     PatientsModule,
     AppointmentsModule
   ],
