95de5bb2266477571c446a4249edf83a67c4c79c
Juliano N. Direne <julianodirene@msn.com>
2021-03-16 23:50:05 -0300

    feature/heroku Add SSL to database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,14 +7,13 @@ import { PatientsModule } from './patients/patients.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      type: "postgres",
+      url: process.env.DATABASE_URL,
       entities: ['dist/**/*.entity.js'],
       synchronize: false,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }),
     PatientsModule,
     AppointmentsModule
